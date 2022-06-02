b6c3f2fe68116aa05c370d373cc0f477e792b61c
mikyge2 <103429957+mikyge2@users.noreply.github.com>
2022-06-02 20:18:29 +0300

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of
- * art,"
+ * prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
